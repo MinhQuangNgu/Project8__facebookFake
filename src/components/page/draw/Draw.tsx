a4a655cbfd05565e565e59992de8c54f40c7437f
QuangNM <quangnmhe160393@fpt.edu.vn>
2023-04-12 16:51:45 +0700

    updating

--- src/components/page/draw/Draw.tsx
+++ src/components/page/draw/Draw.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./style.scss";
 import DrawCard from "./DrawCard";
 const Draw: React.FC = () => {
+	const [item, setItem] = useState(false);
 	return (
 		<div className="draw">
 			<div className="draw__wrap">
@@ -11,6 +12,27 @@ const Draw: React.FC = () => {
 					<DrawCard />
 				</div>
 			</div>
+			{item && (
+				<div className="draw__viewAll">
+					<div className="draw__viewAll__a">
+						<div className="draw__viewAll__image">
+							<img
+								src="https://anhcuoiviet.vn/wp-content/uploads/2022/11/background-dep-0.jpg"
+								alt="Ảnh"
+							/>
+						</div>
+						<div className="draw__viewAll__comment"></div>
+					</div>
+				</div>
+			)}
+			{item && (
+				<div
+					onClick={() => {
+						setItem(false);
+					}}
+					className="draw__viewAll__abs"
+				></div>
+			)}
 		</div>
 	);
 };
