14517af10c7511f0f9e9b93eae373f472857df9c
QuangNM <quangnmhe160393@fpt.edu.vn>
2023-04-13 20:33:01 +0700

    updating

--- src/components/page/profile/Profile.tsx
+++ src/components/page/profile/Profile.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
+import NewFeedCard from "../../card/NewFeedCard";
 type Props = {};
 
 const Profile: React.FC = (props: Props) => {
+	const [touchCard, setTouchCard] = useState<number>(-1);
 	return (
 		<div className="profile">
 			<div className="profile__wrap">
@@ -78,6 +80,134 @@ const Profile: React.FC = (props: Props) => {
 						<div className="profile__user__navbar__items active">Bài viết</div>
 						<div className="profile__user__navbar__items">Bạn bè</div>
 					</div>
+				</div>
+			</div>
+			<div className="padding__profile profile__detail">
+				<div className="profile__detail__1">
+					<div className="border__shadow profile__detail__1__introduction">
+						<div className="__title">Giới thiệu</div>
+						<div className="__description">Anh quang ơi đẹp tria quá</div>
+						<div className="__button">
+							<button>Chỉnh sửa giới thiệu</button>
+						</div>
+					</div>
+					<div className="border__shadow profile__detail__1__introduction">
+						<div className="__title">Ảnh</div>
+						<div className="__image__wrap">
+							<img
+								className="__img"
+								src="https://treobangron.com.vn/wp-content/uploads/2022/09/background-dep-17.jpg"
+								alt="Ảnh"
+							/>
+							<img
+								className="__img"
+								src="https://treobangron.com.vn/wp-content/uploads/2022/09/background-dep-17.jpg"
+								alt="Ảnh"
+							/>
+							<img
+								className="__img"
+								src="https://treobangron.com.vn/wp-content/uploads/2022/09/background-dep-17.jpg"
+								alt="Ảnh"
+							/>
+							<img
+								className="__img"
+								src="https://treobangron.com.vn/wp-content/uploads/2022/09/background-dep-17.jpg"
+								alt="Ảnh"
+							/>
+						</div>
+						<div className="__button">
+							<button>Xem tất cả</button>
+						</div>
+					</div>
+					<div className="border__shadow profile__detail__1__introduction sticky">
+						<div className="__title">Bạn bè</div>
+						<div className="__image__wrap">
+							<div className="__image__card">
+								<img
+									src="https://treobangron.com.vn/wp-content/uploads/2022/09/background-dep-17.jpg"
+									alt="Ảnh"
+								/>
+								<div> Minh Quang</div>
+							</div>
+							<div className="__image__card">
+								<img
+									src="https://treobangron.com.vn/wp-content/uploads/2022/09/background-dep-17.jpg"
+									alt="Ảnh"
+								/>
+								<div>Minh Quang</div>
+							</div>
+							<div className="__image__card">
+								<img
+									src="https://treobangron.com.vn/wp-content/uploads/2022/09/background-dep-17.jpg"
+									alt="Ảnh"
+								/>
+								<div> Minh Quang</div>
+							</div>
+							<div className="__image__card">
+								<img
+									src="https://treobangron.com.vn/wp-content/uploads/2022/09/background-dep-17.jpg"
+									alt="Ảnh"
+								/>
+								<div>Nguyen Minh Quang</div>
+							</div>
+							<div className="__image__card">
+								<img
+									src="https://treobangron.com.vn/wp-content/uploads/2022/09/background-dep-17.jpg"
+									alt="Ảnh"
+								/>
+								<div>Nguyen Minh Quang</div>
+							</div>
+							<div className="__image__card">
+								<img
+									src="https://treobangron.com.vn/wp-content/uploads/2022/09/background-dep-17.jpg"
+									alt="Ảnh"
+								/>
+								<div>Nguyen Minh Quang</div>
+							</div>
+							<div className="__image__card">
+								<img
+									src="https://treobangron.com.vn/wp-content/uploads/2022/09/background-dep-17.jpg"
+									alt="Ảnh"
+								/>
+								<div>Minh Quang</div>
+							</div>
+
+							<div className="__image__card">
+								<img
+									src="https://treobangron.com.vn/wp-content/uploads/2022/09/background-dep-17.jpg"
+									alt="Ảnh"
+								/>
+								<div>Nguyen Minh Quang</div>
+							</div>
+							<div className="__image__card">
+								<img
+									src="https://treobangron.com.vn/wp-content/uploads/2022/09/background-dep-17.jpg"
+									alt="Ảnh"
+								/>
+								<div>Nguyen Minh Quang</div>
+							</div>
+						</div>
+						<div className="__button">
+							<button>Xem tất cả</button>
+						</div>
+					</div>
+				</div>
+				<div className="profile__detail__2">
+					<NewFeedCard
+						index={0}
+						touchCard={touchCard}
+						setTouchCard={setTouchCard}
+					/>
+					<NewFeedCard
+						index={2}
+						touchCard={touchCard}
+						setTouchCard={setTouchCard}
+					/>
+					<NewFeedCard
+						index={3}
+						touchCard={touchCard}
+						setTouchCard={setTouchCard}
+					/>
 				</div>
 			</div>
 		</div>
