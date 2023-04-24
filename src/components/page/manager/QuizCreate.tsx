5017ad5dc538f3ba76bc1f68a5e6b406cb3f262c
QuangNM <quangnmhe160393@fpt.edu.vn>
2023-04-24 15:54:15 +0700

    updating

--- src/components/page/manager/QuizCreate.tsx
+++ src/components/page/manager/QuizCreate.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState, useCallback, useEffect } from "react";
 import "./style.scss";
 import QuizAnswerManager from "./components/QuizAnswerManager";
+import { useNavigate } from "react-router-dom";
 type Props = {};
 interface answers {
 	image?: File;
@@ -8,7 +9,13 @@ interface answers {
 	url?: string;
 }
 const QuizCreate: React.FC = (props: Props) => {
+	//other
+	const navigate = useNavigate();
+	const [select, setSelect] = useState<boolean>(false);
+
+	//end other
 	//question
+	const [time, setTime] = useState<number>(60);
 	const contentRef = useRef<HTMLDivElement>(null);
 	const [content, setContent] = useState<string>("");
 	const [image, setImage] = useState<string>("");
@@ -120,6 +127,17 @@ const QuizCreate: React.FC = (props: Props) => {
 
 	return (
 		<div className="quizCreate">
+			<div className="quizCreate__title">
+				<div className="quizCreate__title__items">Danh sách câu hỏi</div>
+				<div
+					onClick={() => {
+						navigate(-1);
+					}}
+					className="quizCreate__title__items"
+				>
+					Quay lại trang trước
+				</div>
+			</div>
 			<div className="quizCreateForm">
 				<div className="quizCreateForm__head">
 					<div className="quizCreateForm__head__1">
@@ -251,6 +269,115 @@ const QuizCreate: React.FC = (props: Props) => {
 					<img src={imageShow} alt="Ảnh" />
 				</div>
 			)}
+			<div className="quizCreate__tool">
+				<div className="quizCreate__select">
+					<div
+						onClick={() => {
+							setSelect(!select);
+						}}
+						className="quizCreate__select__form"
+					>
+						{time} giây <i className="fa-solid fa-sort-down"></i>
+					</div>
+					{select && (
+						<div className="quizCreate__select__more">
+							<div
+								onClick={() => {
+									setSelect(false);
+									setTime(5);
+								}}
+								className="quizCreate__select__items"
+							>
+								5 giây
+							</div>
+							<div
+								onClick={() => {
+									setSelect(false);
+									setTime(15);
+								}}
+								className="quizCreate__select__items"
+							>
+								15 giây
+							</div>
+							<div
+								onClick={() => {
+									setSelect(false);
+									setTime(30);
+								}}
+								className="quizCreate__select__items"
+							>
+								30 giây
+							</div>
+							<div
+								onClick={() => {
+									setSelect(false);
+									setTime(45);
+								}}
+								className="quizCreate__select__items"
+							>
+								45 giây
+							</div>
+							<div
+								onClick={() => {
+									setSelect(false);
+									setTime(60);
+								}}
+								className="quizCreate__select__items"
+							>
+								60 giây
+							</div>
+							<div
+								onClick={() => {
+									setSelect(false);
+									setTime(120);
+								}}
+								className="quizCreate__select__items"
+							>
+								120 giây
+							</div>
+							<div
+								onClick={() => {
+									setSelect(false);
+									setTime(150);
+								}}
+								className="quizCreate__select__items"
+							>
+								150 giây
+							</div>
+							<div
+								onClick={() => {
+									setSelect(false);
+									setTime(200);
+								}}
+								className="quizCreate__select__items"
+							>
+								200 giây
+							</div>
+							<div
+								onClick={() => {
+									setSelect(false);
+									setTime(250);
+								}}
+								className="quizCreate__select__items"
+							>
+								250 giây
+							</div>
+							<div
+								onClick={() => {
+									setSelect(false);
+									setTime(300);
+								}}
+								className="quizCreate__select__items"
+							>
+								300 giây
+							</div>
+						</div>
+					)}
+				</div>
+				<button className="quizCreate__btn">
+					<i className="fa-solid fa-floppy-disk"></i> Lưu
+				</button>
+			</div>
 		</div>
 	);
 };
